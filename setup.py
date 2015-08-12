4b645268c479b16374ac8cbc3670a04cc0d9ca47
Dmitry Shachnev <mitya57@gmail.com>
2015-08-12 19:00:02 +0300

    Prepare 0.1 release
    This closes #5.

--- setup.py
+++ setup.py
@@ -2,8 +2,22 @@
 
 from distutils.core import setup
 
+long_description = \
+"""This extension adds math formulas support to Python-Markdown_
+(works with version 2.6 or newer).
+
+.. _Python-Markdown: https://github.com/waylan/Python-Markdown
+
+You can find the source on GitHub_.
+Please refer to the `README file`_ for details on how to use it.
+
+.. _GitHub: https://github.com/mitya57/python-markdown-math
+.. _`README file`: https://github.com/mitya57/python-markdown-math/blob/master/README.md
+"""
+
 setup(name='python-markdown-math',
       description='Math extension for Python-Markdown',
+      long_description=long_description,
       author='Dmitry Shachnev',
       author_email='mitya57@gmail.com',
       version='0.1',
