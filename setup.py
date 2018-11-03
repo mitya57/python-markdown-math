fd2c27517621c49f269b77392d55dbebf936b0d0
Dmitry Shachnev <mitya57@gmail.com>
2018-11-03 10:48:06 +0300

    Switch to declarative setup configuration

--- setup.py
+++ setup.py
@@ -2,22 +2,4 @@
 
 from setuptools import setup
 
-with open('README.md') as readme_file:
-    long_description = readme_file.read()
-
-setup(name='python-markdown-math',
-      description='Math extension for Python-Markdown',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Dmitry Shachnev',
-      author_email='mitya57@gmail.com',
-      version='0.6',
-      url='https://github.com/mitya57/python-markdown-math',
-      py_modules=['mdx_math'],
-      install_requires=['Markdown>=3.0'],
-      entry_points={
-          'markdown.extensions': [
-              'mdx_math = mdx_math:MathExtension',
-          ],
-      },
-      license='BSD')
+setup()
